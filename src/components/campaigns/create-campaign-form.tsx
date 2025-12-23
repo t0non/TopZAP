3981327826e168d893f8b5d8747c0cb741bb71d5
Eduardo Tonon <lynselite@gmail.com>
2025-12-23 23:37:08 +0000

    data de envio não funciona

--- src/components/campaigns/create-campaign-form.tsx
+++ src/components/campaigns/create-campaign-form.tsx
@@ -190,7 +190,11 @@ export function CreateCampaignForm() {
                             mode="single"
                             selected={field.value}
                             onSelect={field.onChange}
-                            disabled={(date) => date < new Date() || date < new Date('1900-01-01')}
+                            disabled={(date) => {
+                                const today = new Date();
+                                today.setHours(0, 0, 0, 0);
+                                return date < today;
+                            }}
                             initialFocus
                             locale={ptBR}
                           />
