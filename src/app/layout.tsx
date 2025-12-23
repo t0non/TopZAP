c44c04eef610aa9cfe94830ae56267ca530ed50d
Eduardo Tonon <lynselite@gmail.com>
2025-12-23 23:38:37 +0000

    o calendario está bugado, quando eu seleciono uma data ele não "selecion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import 'react-day-picker/dist/style.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 
